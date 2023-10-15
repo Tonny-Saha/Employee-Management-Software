fb3b33885862adafa01b6409128780282331df99
Prioti Saha <tonnysaha15@gmail.com>
2023-10-15 15:56:26 +0600

    complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Dashboard from './Page'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+       <Dashboard/>
+      </div>
+
+  )
+}
+
+export default App
